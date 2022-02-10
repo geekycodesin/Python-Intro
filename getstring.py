e97ae958b822019eb4a8fc265117839981d1dced
geekycodesin <99415097+geekycodesin@users.noreply.github.com>
2022-02-10 21:29:59 +0530

    Add files via upload

---
+++ getstring.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 21:28:07 2022
+
+@author: ved.prakash
+"""
+
+
+# template begins
+#####################################
+
+# import libraries for input/ output handling
+# on generic level
+import atexit, io, sys
+
+# A stream implementation using an in-memory bytes
+# buffer. It inherits BufferedIOBase.
+buffer = io.BytesIO()
+sys.stdout = buffer
+
+# print via here
+@atexit.register
+def write():
+	sys.stdout.write(buffer.getvalue())
+
+#####################################
+# template ends
+
+# normal method followed
+# input N
+n = int(input())
+
+# input the array
+arr = [int(x) for x in input().split()]
+
+# initialize variable
+summation = 0
+
+# calculate sum
+for x in arr:
+	summation += x
+
+# print answer
+print(summation)
